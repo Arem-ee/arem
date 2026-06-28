20646d03a23c83ca22920eda1ff7e5cfba5ee070
Arem <toromadeadesina@gmail.com>
2026-06-28 14:49:37 -0700

    fixed contact form

--- src/data/socials.ts
+++ src/data/socials.ts
@@ -1,8 +1,8 @@
 export const socialLinks = {
   github: "https://github.com/Arem-ee",
   linkedin: "https://www.linkedin.com/in/toromadeabdulrahman",
-  twitter: "https://x.com/alexmorgan",
-  email: "hello@arem.dev",
+  twitter: "https://x.com/Arem_ee",
+  email: "toromadeadesina@gmail.com",
   portfolio: "https://arem.dev",
   githubProfile: "https://github.com/Arem-ee",
   githubUsername: "Arem-ee",
